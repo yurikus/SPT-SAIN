3d6003f12c55f74679fb763abb4d9037ed6526d9
SleepingPills <SleepingPills@users.noreply.github.com>
2026-01-02 12:57:03 +0000

    Reduce the minimum amount of time that bots stay in combat.

--- SAIN/Classes/Bot/Info/SAINBotInfoClass.cs
+++ SAIN/Classes/Bot/Info/SAINBotInfoClass.cs
@@ -139,12 +139,9 @@ public class SAINBotInfoClass : BotComponentClassBase
         }
 
         TimeBeforeSearch = searchTime;
-        float random = 30f.Randomize(0.75f, 1.25f).Round100();
+        // Get a random time between 20s and 60s
+        float random = 20f.Randomize(1f, 3f).Round100();
         float forgetTime = searchTime + random;
-        if (forgetTime < 240f)
-        {
-            forgetTime = 240f.Randomize(0.9f, 1.1f).Round100();
-        }
 
         BotOwner.Settings.FileSettings.Mind.TIME_TO_FORGOR_ABOUT_ENEMY_SEC = forgetTime;
         ForgetEnemyTime = forgetTime;
